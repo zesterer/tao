4387b4c4b649b43c9f52406c0a8e9eeb832767fa
Capucho <jcapucho7@gmail.com>
2020-05-25 21:26:09 +0100

    Fixed build script

--- playground/deploy.sh
+++ playground/deploy.sh
@@ -2,6 +2,7 @@ git submodule init &&
 git submodule update &&
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y &&
 source $HOME/.cargo/env &&
+cd playground &&
 rustup override set nightly &&
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh &&
 npm install &&
