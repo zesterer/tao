5cb6fd34bdca4317ba87186d9ae895ed6712829e
Capucho <jcapucho7@gmail.com>
2020-05-25 13:11:53 +0100

    Remove unnecessary prints

--- src/lib.rs
+++ src/lib.rs
@@ -19,23 +19,12 @@ pub fn run_module(src: &str) -> Result<vm::Value, Vec<Error>> {
     let ast = ast::parse_module(&tokens)?;
     let hir_prog = hir::Program::new_root(&ast)?;
 
-    //println!("{:?}", hir_prog.root().def(LocalIntern::new("find_char".to_string())));
-
     // TODO: Get rid of this
     let main_ident = LocalIntern::new("main".to_string());
-    if let Some(main) = hir_prog.root().def(main_ident) {
-        println!("TYPE: {}", **main.body.ty());
-    }
 
     let mir_prog = mir::Program::from_hir(&hir_prog, main_ident).map_err(|e| vec![e])?;
 
-    for global in mir_prog.globals() {
-        //println!("GLOBAL: {:#?}", global);
-    }
-
     let prog = mir_prog.compile().map_err(|e| vec![e])?;
-
-    println!("{:?}", prog);
 
     Ok(vm::Vm::default().execute(&prog))
 }
