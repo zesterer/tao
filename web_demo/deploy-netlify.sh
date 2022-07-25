9103f86ca7a4cd50e0d5b206a687853e4904c7d6
Joshua Barretto <joshua.s.barretto@gmail.com>
2022-07-25 21:32:13 +0100

    More details

--- web_demo/deploy-netlify.sh
+++ web_demo/deploy-netlify.sh
@@ -1,7 +1,6 @@
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-wasm-pack build --target bundler --out-dir site/dist
+wasm-pack build --target bundler
 cd site
 npm install --save-dev webpack
-npm install
 npm run build
 cd ..
