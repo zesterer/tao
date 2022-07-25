f2f2daa119a8e35468bdb81b39aeaf869c2bbb51
Joshua Barretto <joshua.s.barretto@gmail.com>
2022-07-25 19:34:35 +0100

    Web dir fix

--- web_demo/deploy-netlify.sh
+++ web_demo/deploy-netlify.sh
@@ -1,5 +1,5 @@
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-wasm-pack build --target bundler --out-dir www/dist
-cd www
+wasm-pack build --target bundler --out-dir site/dist
+cd site
 npm run build
 cd ..
