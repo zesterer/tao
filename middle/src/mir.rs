59db9e1c335ce94e9860c8f9425519e4cd3fa2dd
Matthias Beyer <mail@beyermatthias.de>
2023-03-10 16:09:34 +0100

    Revert "Add MPL-2.0 header to all .rs files"
    This reverts commit 294e627875bb40546f484ed81e377c640dee9f57.
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- middle/src/mir.rs
+++ middle/src/mir.rs
@@ -1,7 +1,3 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 //! The MIR is *not* correct (in the context of Tao's abstract machine) by construction. See the 'SAFETY' notes.
 
 use super::*;
