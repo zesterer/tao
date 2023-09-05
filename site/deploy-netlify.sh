c627719910d58797efbd75c05806a1e3844a5245
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-09-05 17:05:26 +0100

    Fix deploy

--- site/deploy-netlify.sh
+++ site/deploy-netlify.sh
@@ -1,3 +1,3 @@
 cd site
-wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
-trunk build
+wget -qO- https://github.com/thedodd/trunk/releases/download/v0.17.5/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+./trunk build
